02431f26e00758a005a99e1e97e41073154bdd44
Stephen Roberts <stephen@offroadcode.com>
2014-07-01 11:58:10 +0100

    Added Html Helper to cache partials

---
+++ UmbCacheDependencies/HtmlHelperRenderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Lucene.Net.Util.Cache;
+using Umbraco.Core;
+using umbraco.cms.businesslogic.member;
+using Umbraco.Web;
+using Cache = System.Web.Caching.Cache;
+
+
+namespace UmbCacheDependencies
+{
+    public static class HtmlHelperRenderExtensions
+    {
+        public static IHtmlString DependencyCachedPartial(this HtmlHelper htmlHelper, string key, string partialViewName, object model, UmbracoDependency dependencies, ViewDataDictionary viewData = null)
+        {
+            var cache = HttpContext.Current.Cache;
+            var result = cache[key] as MvcHtmlString;
+            if (result == null)
+            {
+                result = htmlHelper.Partial(partialViewName, model, viewData);
+                cache.Add(key, result, dependencies, Cache.NoAbsoluteExpiration, Cache.NoSlidingExpiration,
+                    CacheItemPriority.Normal, null);
+            }
+            return result;
+        }
+    }
+}
